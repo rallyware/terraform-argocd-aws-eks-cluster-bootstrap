a3b078ea9b400d9eb3a44c104102be779e8cf414
Vladimir <26582191+SweetOps@users.noreply.github.com>
2022-04-14 10:41:16 +0300

    chore: bump aws provider lower binding to  =>4.2.0 (#14)

--- versions.tf
+++ versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "< 4.0"
+      version = ">= 4.2.0"
     }
     tls = {
       source  = "hashicorp/tls"
