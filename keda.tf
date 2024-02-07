ae54afcde96e7ebbad52f8ff9ece5ce28596f2ab
Volodymyr S <26582191+SweetOps@users.noreply.github.com>
2024-02-07 13:29:45 -0500

    fix: override default KEDA SA name (#116)

--- keda.tf
+++ keda.tf
@@ -4,6 +4,7 @@ locals {
   keda_iam_policy_enabled         = local.keda_enabled ? local.argocd_helm_apps_set["keda"]["create_default_iam_policy"] : false
   keda_iam_policy_document        = local.keda_iam_policy_enabled ? one(data.aws_iam_policy_document.keda[*].json) : try(local.argocd_helm_apps_set["keda"]["iam_policy_document"], "{}")
   keda_use_sts_regional_endpoints = local.keda_enabled ? local.argocd_helm_apps_set["keda"]["use_sts_regional_endpoints"] : false
+  keda_service_account_name       = try(local.argocd_helm_apps_set["keda"]["name"], "")
 }
 
 module "keda_label" {
@@ -30,7 +31,7 @@ module "keda_eks_iam_role" {
 
   aws_iam_policy_document     = local.keda_iam_policy_document
   eks_cluster_oidc_issuer_url = local.eks_cluster_oidc_issuer_url
-  service_account_name        = try(local.argocd_helm_apps_set["keda"]["name"], "")
+  service_account_name        = local.keda_service_account_name
   service_account_namespace   = try(local.argocd_helm_apps_set["keda"]["namespace"], "")
 
   label_order = var.irsa_label_order
